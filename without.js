1773d0fbb5d47b488b3abc0097ca520b7c5b1c8a
Viron A <virondawith@gmail.com>
2025-07-20 06:31:00 -0400

    Added without function with test cases

---
+++ without.js
@@ -0,0 +1,42 @@
+// Copy of eqArrays function:
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+
+// Copy of assertArraysEqual funtion:
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+// Without function:
+const without = function(source, itemsToRemove) {
+  const result = [];
+
+  for (let item of source) {
+    if (!itemsToRemove.includes(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+
+
+// Tests
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without(["apple", "banana", "cherry"], ["banana"]), ["apple", "cherry"]);
+assertArraysEqual(without([true, false, true], [false]), [true, true]);
