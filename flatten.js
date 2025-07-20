72a8089f8c56cb537fc347020a5ba8240a88a572
Viron A <virondawith@gmail.com>
2025-07-20 07:11:47 -0400

    Added flatten function and test cases

---
+++ flatten.js
@@ -0,0 +1,45 @@
+// Copy of eqArrays function:
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+
+// Copy of assertArraysEqual funtion:
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+// Flatten function:
+const flatten = function(arr) {
+  const result = [];
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      for (let subItem of item) {
+        result.push(subItem);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+
+// Test cases:
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten(["a", ["b", "c"], "d"]), ["a", "b", "c", "d"]);
+assertArraysEqual(flatten([[1, 2], 3, 4]), [1, 2, 3, 4]);
+assertArraysEqual(flatten([1, [2], [3, 4], 5]), [1, 2, 3, 4, 5]);
