3b573e29bc68a6b95e11cd2550ce7cfd126f4a39
Viron A <virondawith@gmail.com>
2025-07-22 09:56:42 -0400

    Built the function to return middle values of a given array

---
+++ middle.js
@@ -0,0 +1,48 @@
+// Copy of eqArrays function:
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+
+// Copy of assertArraysEqual funtion:
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+// ACTUAL FUNCTION
+const middle = function(array) {
+  const len = array.length;
+
+  if (len <= 2) return [];
+
+  const mid = Math.floor(len / 2);
+
+
+  if (len % 2 === 0) {
+    return [array[mid - 1], array[mid]];
+  }
+
+  return [array[mid]];
+};
+
+
+
+// TEST CODE
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle(["a", "b", "c"]), ["b"]);
