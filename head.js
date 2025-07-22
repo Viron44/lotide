02ff4a3ac556f5240c8014e8c5e14b501607cef0
Viron A <virondawith@gmail.com>
2025-07-22 09:33:48 -0400

    Added test code comment

--- head.js
+++ head.js
@@ -10,6 +10,8 @@ const head = function(array) {
   return array[0];
 };
 
+
+// TEST CODE:
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([10]), 10);
