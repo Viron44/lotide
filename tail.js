dfd1065cbfd40a0d4c02b57a5f27681c06771ad8
Viron A <virondawith@gmail.com>
2025-07-29 21:36:46 -0400

    Update tail with comments.js

--- tail.js
+++ tail.js
@@ -1,3 +1,4 @@
+// assertEqual function:
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,10 +7,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// tail function:
 const tail = function(array) {
   return array.slice(1);
 };
 
 
+// TEST CODE:
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result[0], "Lighthouse");
